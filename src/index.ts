cba784c38c99cc1dbd538089b43d7a1e69125b96
wrule <1982775886@qq.com>
2024-07-12 23:15:10 +0800

    复制过来代码

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,75 @@
+import crypto from 'crypto';
+import SQLite3, { Database, RunResult } from 'sqlite3';
 
 export
-function hello() {
-  console.log('hello world!');
+function add(db: Database, key: string, value: string) {
+  return new Promise<RunResult>((resolve, reject) => {
+    const time = Date.now();
+    db.run(
+      `INSERT INTO ktv(key, time, value) VALUES(?, ?, ?)`,
+      [key, time, value],
+      (result: RunResult, error: Error) => {
+        if (error) reject(error);
+        else resolve(result);
+      },
+    );
+  });
+}
+
+export
+function get(db: Database, key: string) {
+  return new Promise<string>((resolve, reject) => {
+    db.get(`SELECT value FROM ktv WHERE key = ?`, [key], (error: Error, row: any) => {
+      if (error) reject(error);
+      else resolve(row.value);
+    });
+  });
+}
+
+export
+function del(db: Database, key: string) {
+  return new Promise<RunResult>((resolve, reject) => {
+    db.run(
+      `DELETE FROM ktv WHERE key = key`,
+      [key],
+      (result: RunResult, error: Error) => {
+        if (error) reject(error);
+        else resolve(result);
+      },
+    );
+  });
+}
+
+export
+function set(db: Database, key: string, value: string) {
+  return new Promise<RunResult>((resolve, reject) => {
+    const time = Date.now();
+    db.run(
+      `INSERT OR REPLACE INTO ktv(key, time, value) VALUES(?, ?, ?)`,
+      [key, time, value],
+      (result: RunResult, error: Error) => {
+        if (error) reject(error);
+        else resolve(result);
+      },
+    );
+  });
+}
+
+export
+async function hello() {
+  const sqlite3 = SQLite3.verbose();
+  const db = new sqlite3.Database('test/1.db');
+  // db.serialize(() => {
+  //   const stmt = db.prepare(`INSERT INTO ktv(key, time, value, mtime) VALUES(?, ?, ?, ?)`);
+  //   for (let i = 0; i < 10; i++) {
+  //     stmt.run(crypto.randomUUID(), Date.now(), (i + 1).toString(), Date.now());
+  //   }
+  //   stmt.finalize();
+
+  //   db.each('SELECT * FROM ktv', (err, row: any) => {
+  //     console.log(row);
+  //   });
+  // });
+  console.log(await set(db, 'jimao', '大傻逼1'));
+  db.close();
 }
